32205199189c198d2dfd53e509d8458595510d8b
Ananya114 <gananya114@gmail.com>
2022-09-13 17:00:39 +0530

    Commiting HelloWorld

--- src/HelloWorldPrinter.java
+++ src/HelloWorldPrinter.java
@@ -1,19 +1,6 @@
-import java.util.Scanner;
 
 public class HelloWorldPrinter {
-    public static void main(String[] args){
-        String day;
-        Scanner sc=new Scanner(System.in);
-        day= sc.nextLine();
-        switch(day){
-            case "Sunday":
-                System.out.println("Holiday");
-                break;
-            case "Monday":
-                System.out.println("Working day");
-                break;
-            case "Saturday":
-                System.out.println("Holiday");
-        }
+    public static void main(String[] args) {
+        System.out.println("My name is Ananya");
     }
 }
