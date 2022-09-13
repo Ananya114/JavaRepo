b4a1dcd97431ceef8f46e93440847cd2d01c6c9e
Ananya114 <gananya114@gmail.com>
2022-09-13 16:41:34 +0530

    Commiting HelloWorldPrinter

---
+++ src/HelloWorldPrinter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HelloWorldPrinter {
+    public static void main(String[] args){
+        String day;
+        Scanner sc=new Scanner(System.in);
+        day= sc.nextLine();
+        switch(day){
+            case "Sunday":
+                System.out.println("Holiday");
+                break;
+            case "Monday":
+                System.out.println("Working day");
+                break;
+        }
+    }
+}
