f26aab32539a78e783b8dedf14169506d3b8b550
Ananya114 <gananya114@gmail.com>
2022-09-14 11:05:55 +0530

    Commiting DoraMangoTree

--- src/DoraMangoTree.java
+++ src/DoraMangoTree.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class DoraMangoTree {
+import java.util.Scanner;
+
+public class DoraMangoTree {
+    public static void main(String[] args) {
+        int row, col, tree;
+        Scanner sc=new Scanner(System.in);
+        row=sc.nextInt();
+        col= sc.nextInt();
+        tree=sc.nextInt();
+        if (tree>=1&&tree<=col)
+            System.out.println("It's a Mango tree");
+        else if ((tree-1)%col==0)
+            System.out.println("It's a Mango tree");
+        else if (tree%col==0)
+            System.out.println("It's a Mango tree");
+        else
+            System.out.println("It's not a Mango tree");
+    }
 }
--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class LeapYear {
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year;
+        Scanner sc=new Scanner(System.in);
+        year=sc.nextInt();
+        if (year%400==0||(year%4==0&&year%100!=0))
+            System.out.println("It's a leap year");
+        else
+            System.out.println("It's not a leap year");
+    }
 }
