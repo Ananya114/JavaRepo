4c9a038a5831d6d254fc88fe93e0c14c1eeea7a5
Ananya114 <gananya114@gmail.com>
2022-09-15 11:01:09 +0530

    Commiting AlphabetPattern

--- src/AlphabetPattern.java
+++ src/AlphabetPattern.java
@@ -1,5 +1,11 @@
 public class AlphabetPattern {
     public static void main(String[] args) {
-        System.out.println("A\nAB\nABC\nABCD\nABCDE");
+        int n=5;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print((char)(j+64));
+            }
+            System.out.println();
+        }
     }
 }
